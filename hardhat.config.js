4325b9a85c45f2af6f441f4c097495b473056951
a6Ce6Bs <perodyz@protonmail.com>
2022-09-04 20:19:15 +0200

    add: hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.7',
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: process.env.MUMBAI_ALCHEMY_RPC_URL,
+      chainId: 80001,
+      accounts: [process.env.DEPLOY_PRIVATE_KEY],
+    },
+    polygon: {
+      url: process.env.POLYGON_ALCHEMY_RPC_URL,
+      chainId: 137,
+      accounts: [process.env.DEPLOY_PRIVATE_KEY],
+    },
+  },
+}
