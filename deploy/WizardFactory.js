bcb66f5d463729d10d175c85e1b7007dd862384d
a6Ce6Bs <perodyz@protonmail.com>
2022-09-09 11:46:45 +0200

    add: networkConfig - priceFeedAddress

--- deploy/WizardFactory.js
+++ deploy/WizardFactory.js
@@ -1,11 +1,13 @@
 const { cost } = require('../arguments')
 const { verify } = require('../scripts/verify')
+const { networkConfig } = require('../networkConfig')
 
 module.exports = async ({ getNamedAccounts, deployments, run, network }) => {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
-  const localhost = network.config.chainId == 31337
+  const chainId = network.config.chainId
+  const localhost = chainId == 31337
 
   const MockV3Aggregator = await deployments.get('MockV3Aggregator')
   const MockV3AggregatorAddress = MockV3Aggregator.address
@@ -13,7 +15,12 @@ module.exports = async ({ getNamedAccounts, deployments, run, network }) => {
   const WizardFactory = await deploy('WizardFactory', {
     contract: 'WizardFactory',
     from: deployer,
-    args: [cost, MockV3AggregatorAddress],
+    args: [
+      cost,
+      localhost
+        ? MockV3AggregatorAddress
+        : networkConfig[chainId].priceFeedAddress,
+    ],
     log: true,
   })
 
---
+++ networkConfig.js
@@ -0,0 +1,12 @@
+const networkConfig = {
+  137: {
+    name: 'polygon',
+    priceFeedAddress: '0xAB594600376Ec9fD91F8e885dADF0CE036862dE0',
+  },
+  80001: {
+    name: 'mumbai',
+    priceFeedAddress: '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada',
+  },
+}
+
+module.exports = { networkConfig }
