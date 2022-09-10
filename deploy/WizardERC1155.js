fa9138bdd5c199f135755b5576a95c0d0f02f19b
a6Ce6Bs <perodyz@protonmail.com>
2022-09-10 15:26:06 +0200

    fix: deploy - waitConfirmations localhost

--- deploy/WizardERC1155.js
+++ deploy/WizardERC1155.js
@@ -11,7 +11,7 @@ module.exports = async ({ getNamedAccounts, deployments, run, network }) => {
     from: deployer,
     args: [],
     log: true,
-    waitConfirmations: 5,
+    waitConfirmations: localhost ? 0 : 5,
   })
 
   if (!localhost) {
